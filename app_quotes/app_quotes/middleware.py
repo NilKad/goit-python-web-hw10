d429cdc345404e061bfd67680afe3885d886f7fd
NilKad <kadulin@gmail.com>
2024-05-25 15:02:20 +0200

    error delete

--- app_quotes/app_quotes/middleware.py
+++ app_quotes/app_quotes/middleware.py
@@ -9,11 +9,18 @@ class HttpMethodOverrideMiddleware:
         print("!!!!!!!!!!!! - midlware")
         if request.method == "POST" and "_method" in request.POST:
             print("!!!! - mehod POST")
+            print(f"request.POST: {request.POST}")
+
+            # print(f"request.DELETE: {request.DELETE}")
             request.method = request.POST["_method"].upper()
             # csrf_token = get_token(request)
             # request.META["X-CSRFToken"] = csrf_token
             print(f"!!!!! new methot: {request.method}")
+            request.DELETE = request.POST
+            
         response = self.get_response(request)
 
         # return self.get_response(request)
         return response
+
+
